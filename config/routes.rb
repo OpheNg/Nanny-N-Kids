e8756b812ad8caff0ef76af2f1b2211bf8321a12
ulysse galet <ulysse.galet@rennes-sb.com>
2022-08-25 21:08:33 +0200

    V1 - Controller booking (Index) + création d'une seed

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,15 @@ Rails.application.routes.draw do
   # resources :user
   resources :nanny
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # resources :booking
+  # get 'booking', to: 'users#booking'
+
+  # root "bookings#index"
+
+  resources :bookings do
+    resources :nannies
+    resources :users
+  end
+
 end
