31f3b53d4de96276814bbc09f0e7d9c6c828eccb
ulysse galet <ulysse.galet@rennes-sb.com>
2022-08-26 23:13:42 +0200

    Création début d'OK

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
 
     def index
+        @user = User.find(params[:user_id])
         @bookings = Booking.all
     end
 
@@ -8,18 +9,40 @@ class BookingsController < ApplicationController
         @booking = Booking.find(params[:id])
     end
 
+    #Ne fonctionne pas
     def new
+        @user = User.find(params[:user_id])
         @booking = Booking.new
     end
 
+    #Ne fonctionne pas
     def create 
         @booking = Booking.new(booking_params)
 
+        @user = User.find(params[:user_id])
+        @booking.user = @user
+
         if @booking.save
+            redirect_to user_booking_path(@booking.user.id, @booking.id)
+        else
+        end
+    end
+
+
+    def edit
+        @booking = Booking.find(params[:id])
+    end
+
+    def update
+        @booking = Booking.find(params[:id])
+
+        if @booking.update(booking_params_update)
             redirect_to @booking
         else
         end
     end
+
+
 
     private
 
