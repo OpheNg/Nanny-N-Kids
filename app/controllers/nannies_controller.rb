907e0819599f225b78dcd419d79f82a9961d18f6
ulysse galet <ulysse.galet@rennes-sb.com>
2022-08-28 17:56:21 +0200

    changement de routes OK, possibilité de créer 1 ou plusieurs nannies en tant que user, redirection vers index nannies ou tout est visible, bookings toujours ok mais pas connecté, seed un peu amélioré mais ne fonctionne pas vraiment

--- app/controllers/nannies_controller.rb
+++ app/controllers/nannies_controller.rb
@@ -1,2 +1,36 @@
 class NanniesController < ApplicationController
+
+
+    def index
+        @user = current_user
+        @nannies = Nanny.all
+        @nannies = policy_scope(Nanny).order(created_at: :desc)
+    end
+
+    def new
+        @user = current_user
+        @nanny = Nanny.new
+        authorize @nanny
+    end
+
+    def create
+        @nanny = Nanny.new(nanny_params)
+
+        @user = current_user
+        @nanny.user = current_user
+        authorize @nanny
+
+        if @nanny.save
+            redirect_to nannies_path
+        else
+        end 
+    end 
+
+    private 
+
+    def nanny_params
+        params.require(:nanny).permit(:first_name, :last_name, :tel, :price_per_day)
+    end 
+
+
 end
