38e3af06151e790862d3580610d5857b6cdb7b9f
OpheNg <ophe.ng@live.fr>
2022-09-02 12:32:38 +0200

    renommage de certains champs, user way et non pas developper way

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,86 +1,74 @@
 class BookingsController < ApplicationController
+  def index
+    @user = current_user
+    @bookings = Booking.all
+    @bookings = policy_scope(Booking).order(created_at: :desc)
+  end
 
-    def index
+  def show
+    @user = current_user
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
 
-        @user = current_user
-        @bookings = Booking.all
-        @bookings = policy_scope(Booking).order(created_at: :desc)
+  def new
+    @nanny = Nanny.find(params[:nanny_id])
+    @user = current_user
+    @booking = Booking.new
+    authorize @booking
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+
+    @user = current_user
+    @booking.user = current_user
+    authorize @booking
+
+      #Condition pour ne pas créer des réservations aujourd'hui ou plus tard et date de fin après date départ
+      if @booking.start_date >= Date.today && @booking.start_date <= @booking.end_date
+          @booking.save
+          redirect_to bookings_path
+      else
+      end
+  end
+
+  def edit
+    @user = current_user
+    @booking = Booking.find(params[:format])
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+
+    @user = current_user
+    @booking.user = @user
+    authorize @booking
+
+    if @booking.update(booking_params_update)
+      redirect_to booking_path
+    else
     end
+  end
 
-    def show
-        
-        @user = current_user
-        @booking = Booking.find(params[:id])
-        authorize @booking
+  def destroy
+    @booking = Booking.find(params[:format])
+    authorize @booking
 
-        
+    if @booking.destroy
+      redirect_to bookings_path
+    else
     end
-
-    def new
-
-        @nanny = Nanny.find(params[:nanny_id])
-        @user = current_user
-        @booking = Booking.new
-        authorize @booking
-    end
-
-    def create 
-        @booking = Booking.new(booking_params)
-
-        @user = current_user
-        @booking.user = current_user
-        authorize @booking
-
-        #Condition pour ne pas créer des réservations aujourd'hui ou plus tard et date de fin après date départ
-        if @booking.start_date >= Date.today && @booking.start_date <= @booking.end_date
-           @booking.save
-           redirect_to bookings_path
-        else
-        end
-    end
-
-
-    def edit
-        @user = current_user
-        @booking = Booking.find(params[:format])
-        authorize @booking
-    end
-
-    def update
-        @booking = Booking.find(params[:id])
-
-        @user = current_user
-        @booking.user = @user
-        authorize @booking
-
-        if @booking.update(booking_params_update)
-            redirect_to booking_path
-        else
-        end
-    end
-
-    def destroy 
-        @booking = Booking.find(params[:format])
-        authorize @booking
-        
-        if @booking.destroy
-            redirect_to bookings_path
-        else
-        end
-    end 
-
-
+  end
 
     private
 
-    def booking_params
-        params.require(:booking).permit(:end_date, :start_date, :nanny_id)
-    end
+  def booking_params
+    params.require(:booking).permit(:end_date, :start_date, :nanny_id)
+  end
 
-    def booking_params_update
-        params.require(:booking).permit(:end_date)
-    end
-
-
+  def booking_params_update
+    params.require(:booking).permit(:end_date, :start_date)
+  end
 end
